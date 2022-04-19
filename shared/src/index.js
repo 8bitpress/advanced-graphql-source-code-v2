f8d8cb1bc38900be85336b83d9f314b9cdf2404c
Mandi Wise <hello@mandiwise.com>
2022-04-19 10:49:53 -0600

    Export Pagination class from shared module.

--- shared/src/index.js
+++ shared/src/index.js
@@ -1,5 +1,6 @@
 import authDirectives from "./directives/authDirectives.js";
 import DateTimeType from "./scalars/DateTypeType.js";
+import Pagination from "./utils/Pagination.js";
 import restoreReferenceResolvers from "./utils/restoreReferenceResolvers.js";
 
-export { authDirectives, DateTimeType, restoreReferenceResolvers };
+export { authDirectives, DateTimeType, Pagination, restoreReferenceResolvers };
