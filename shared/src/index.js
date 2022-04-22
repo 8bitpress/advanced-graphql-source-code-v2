6bcdc4b4464e30c2f1e555071f03e1a16341bae5
Mandi Wise <hello@mandiwise.com>
2022-04-21 18:17:01 -0600

    Fix typo.

--- shared/src/index.js
+++ shared/src/index.js
@@ -2,5 +2,12 @@ import authDirectives from "./directives/authDirectives.js";
 import DateTimeType from "./scalars/DateTypeType.js";
 import Pagination from "./utils/Pagination.js";
 import restoreReferenceResolvers from "./utils/restoreReferenceResolvers.js";
+import URLType from "./scalars/URLType.js";
 
-export { authDirectives, DateTimeType, Pagination, restoreReferenceResolvers };
+export {
+  authDirectives,
+  DateTimeType,
+  Pagination,
+  restoreReferenceResolvers,
+  URLType
+};
