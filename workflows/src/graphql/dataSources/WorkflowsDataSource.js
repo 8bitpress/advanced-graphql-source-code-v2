388c265ff138a76691b28b515773391aa53b4a1c
Mandi Wise <hello@mandiwise.com>
2022-04-30 20:23:55 -0600

    Add deleteAllUserData mutation.

--- workflows/src/graphql/dataSources/WorkflowsDataSource.js
+++ workflows/src/graphql/dataSources/WorkflowsDataSource.js
@@ -1,8 +1,27 @@
+import { Connection, WorkflowClient } from "@temporalio/client";
 import { DataSource } from "apollo-datasource";
+
+import { DeleteAllUserData } from "../../temporal/workflows.js";
 
 class WorkflowsDataSource extends DataSource {
   constructor() {
     super();
+    const connection = new Connection();
+    this.client = new WorkflowClient(connection.service);
+  }
+
+  async deleteAllUserData(accountId) {
+    try {
+      const handle = await this.client.start(DeleteAllUserData, {
+        taskQueue: "marked-app",
+        workflowId: `delete-user-data-${accountId}`,
+        args: [accountId]
+      });
+      await handle.result();
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
 
--- workflows/src/graphql/resolvers.js
+++ workflows/src/graphql/resolvers.js
@@ -1,3 +1,9 @@
-const resolvers = {};
+const resolvers = {
+  Mutation: {
+    deleteAllUserData(root, { accountId }, { dataSources }) {
+      return dataSources.workflowsAPI.deleteAllUserData(accountId);
+    }
+  }
+};
 
 export default resolvers;
